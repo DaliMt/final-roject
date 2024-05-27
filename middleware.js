da8fb34c7a630ead1013cecf6fecd37015a8d46d
dali <daali.mtibaa@gmail.com>
2024-05-28 00:46:20 +0100

    removing home from middleware

--- middleware.js
+++ middleware.js
@@ -2,12 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export { default } from "next-auth/middleware";
 
-export function middleware(request) {
-  let session = request.cookies.get("next-auth.session-token");
+// export function middleware(request) {
+//   let session = request.cookies.get("next-auth.session-token");
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/home", request.url));
-  }
-}
+//   if (!session) {
+//     return NextResponse.redirect(new URL("/home", request.url));
+//   }
+// }
 
 export const config = { matcher: ["/auth/dashboard", "/"] };
