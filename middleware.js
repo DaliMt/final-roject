e5e29c1024032a1206ba21755d6a68d9b49be8e6
dali <daali.mtibaa@gmail.com>
2024-05-28 01:45:25 +0100

    using token instead of session in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,13 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export { default } from "next-auth/middleware";
 
-// export function middleware(request) {
-//   let session = request.cookies.get("next-auth.session-token");
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-//   if (!session) {
-//     return NextResponse.redirect(new URL("/home", request.url));
-//   }
-// }
+  if (!token) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+}
 
 export const config = { matcher: ["/auth/dashboard", "/"] };
